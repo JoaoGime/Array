fd030b743624c433fedef6628a7424cc8972eef4
João Gimenes <joaopedrossgg@gmail.com>
2025-03-31 08:49:45 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+//Criar o vetor ARRAY
+
+var nomes = ["Jao", "Lipe", "Bruno", "Igor"] 
+
+var nomeEscolhido = parseInt(prompt("Digite um número de 0 a 3"))
+
+//Concatenar 
+document.write("Nome escolhido: " + nomes[nomeEscolhido])
