d82530196c290aa982bf5cb922fc362611b5bbb4
João Gimenes <joaopedrossgg@gmail.com>
2025-03-31 08:52:13 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 //Criar o vetor ARRAY
 
-var nomes = ["Jao", "Lipe", "Bruno", "Igor"] 
+var nomes = ["Joao", "Lipe", "Bruno", "Igor"] 
 
 var nomeEscolhido = parseInt(prompt("Digite um número de 0 a 3"))
 
